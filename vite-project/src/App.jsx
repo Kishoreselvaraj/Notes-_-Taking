42b2f4d6b07b4eb3ef1df6e7057bfef7ef518d79
Kishore Selvaraj <89259172+Kishoreselvaraj@users.noreply.github.com>
2025-02-04 19:53:15 +0530

    .

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter ,Routes, Route} from 'react-router-dom';
 import "./App.css";
 import LoginPage from "./pages/LoginPage.jsx"; // Import the default export
 import Dashboard from "./pages/Dashboard.jsx";
-import Dash from "./pages/Dash2.jsx"
+// import Dash from "./pages/Dash2.jsx"
 // const dotenv=require("dotenv");
 // require('dotenv').config();
 
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} /> {/* Use as a JSX element */}
         <Route path="/dashboard" element={<Dashboard />} /> {/* Use as a JSX element */}
-        <Route path="/dashboard1" element={<Dash />} /> 
+        {/* <Route path="/dashboard1" element={<Dash />} />  */}
       </Routes>
     </BrowserRouter>
   );
